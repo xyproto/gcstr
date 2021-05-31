70e1d17b5dcda301989873140d5b0358d7c270b7
Alexander F. Rødseth <rodseth@gmail.com>
2021-05-31 15:59:44 +0200

    Update example

--- cmd/sleepycat/main.c
+++ cmd/sleepycat/main.c
@@ -3,10 +3,7 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2) {
-        return EXIT_FAILURE;
-    }
-    String* filename = NewString(argv[1]);
+    String* filename = NewString((argc > 1) ? argv[1] : "-");
     Print(MustReadFile(filename));
     return EXIT_SUCCESS;
 }
